e3d1ed3c0c94acebd27c0b4f3a69b18451addc87
John Wiand <JWiand3636@gmail.com>
2018-03-24 02:53:10 -0400

    first commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: "mongodb://MyUser:MyPassword@cluster0-shard-00-00-lgry1.mongodb.net:27017,cluster0-shard-00-01-lgry1.mongodb.net:27017,cluster0-shard-00-02-lgry1.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",
+    database: "mongodb://MyUser:MyPassword@cluster0-shard-00-00-lgry1.mongodb.net:27017,cluster0-shard-00-01-lgry1.mongodb.net:27017,cluster0-shard-00-02-lgry1.mongodb.net:27017/FirstMean?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",
     secret: 'secret'
 }
